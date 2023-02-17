68689717dfc157452a738690dad592a719f08399
CJ Felicitas <cjbfelicitas@usep.edu.ph>
2023-02-17 18:37:22 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@700&display=swap" rel="stylesheet">
+
+
+    <!-- custom css -->
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Felicitas BLOG</title>
+
+    <style>
+        * {
+            font-family: 'Ubuntu', sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <br><br><br>
+
+        <div class="row">
+            <div class="col-md-12">
+                <img src="images/profile_pic.png" class="img-circle img-thumbnail center-content profile-pic-size" alt="">
+                <h1 class="text-center">Cedrick James B. Felicitas</h1>
+                <h5 class="text-center">Student</h5>
+            </div>
+        </div>
+        <br><br><br><br>
+
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="text-center">Blog Entries</h2>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4">
+                <div class="thumbnail">
+                    <a href="/w3images/lights.jpg">
+                        <img src="images/entry1.png" alt="Lights" style="width:100%">
+                        <div class="caption">
+                            <p class="text-center">Entry 1</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="thumbnail">
+                    <a href="images/entry2.png">
+                        <img src="images/entry2.png" alt="Nature" style="width:100%">
+                        <div class="caption">
+                            <p class="text-center">Entry 2</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="thumbnail">
+                    <a href="images/entry3.png">
+                        <img src="images/entry3.png" alt="Fjords" style="width:100%">
+                        <div class="caption">
+                            <p class="text-center">Entry 3</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="row">
+            <div class="col-md-2"></div>
+        <div class="col-md-4">
+                <div class="thumbnail">
+                    <a href="/w3images/lights.jpg">
+                        <img src="images/entry4.png" alt="Lights" style="width:100%">
+                        <div class="caption">
+                            <p class="text-center">Entry 4</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="thumbnail">
+                    <a href="/w3images/lights.jpg">
+                        <img src="images/entry5.png" alt="Lights" style="width:100%">
+                        <div class="caption">
+                            <p class="text-center">Entry 5</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-2"></div>
+        </div>
+
+
+        <h3 class="text-center h1footer">This site is for academic requirements only</h3>
+    </div>
+
+</body>
+
+</html>
